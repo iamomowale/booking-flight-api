29c5f4c9a81042b836cd55061b1ad3ebd8383c29
Omowale Yusuf <yramoni251@stu.ui.edu.ng>
2022-06-25 15:15:08 +0100

    My updated version

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
 const express = require("express");
 const { json } = require("express");
 const flights = require("./controllers/flightController");
-const models = require("./models/Flight");
+const models = require("./models/Flights.json");
 const routes = require("./routes/flightRoute");
 
 const app = express();
 
 app.use(json());
+app.use(express.urlencoded())
 
 app.use("/", routes);
+app.use("/bookflight", routes)
+app.use("/getAllFlights", routes)
+app.use("/getSingleFlight", routes)
+app.use("/deleteFlight", routes)
+app.use('/updateFlight', routes)
+
 
 const port = process.env.PORT || 3000;
 
--- routes/flightRoute.js
+++ routes/flightRoute.js
@@ -3,7 +3,13 @@ const express = require('express');
 const router = express.Router();
 const controller = require('../controllers/flightController');
 
-router.get('/', controller.example)
+router.get('/', controller.homePage)
+router.post('/bookflight', controller.bookFlight)
+router.get('/getAllFlights', controller.getAllFlights)
+router.get('/getSingleFlight/:id', controller.getSingleFlight)
+router.delete('/deleteFlight/:id', controller.deleteFlight)
+router.patch('/updateFlight/:id', controller.updateFlight)
+//Write each routes here and call all CRUD functions from flightcontroller.js
 
 module.exports = router;
 
